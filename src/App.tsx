a045b2fbc5dc62f1aba2a1f2efc806e9a79d63d6
Abdul Rehman <r3hmantom@gmail.com>
2023-04-23 21:24:43 +0500

    Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 const App = () => {
   return (
     <div>
-      <ExpandableText maxChar={10}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
-        necessitatibus asperiores dolorem, eligendi ut ullam repellat atque,
-        iusto doloremque recusandae debitis vero dignissimos quasi ea modi
-        maxime nobis, eos hic consequatur error harum velit quia animi? Vel
-        iusto quo perferendis animi fugiat, necessitatibus aspernatur,
-        laboriosam inventore qui quae commodi ut!
-      </ExpandableText>
+      <Form />
     </div>
   );
 };
